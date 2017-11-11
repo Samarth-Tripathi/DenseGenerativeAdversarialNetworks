8fb9d394fc760414d91df5165c10cfc679ff7940
Renbo <rt2632@columbia.edu>
2017-11-11 15:05:41 -0500

    Inception

--- evaluate.py
+++ evaluate.py
@@ -10,59 +10,130 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import sys
+import torch.utils.data
+from torchvision.models.inception import inception_v3
 
-DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
-softmax = None
+import sys
+import argparse
+import os 
+from PIL import Image 
+from scipy.stats import entropy
+
+#DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataroot', required=True, help='path to the images')
+parser.add_argument('--cuda', default=False, help='whether to use gpu or not')
+opt = parser.parse_args()
+
+#print(opt)
+
+folder = opt.dataroot
+gpu = opt.cuda
+
+def load_images(path):
+	#return image array
+
+	imageList = os.listdir(path)
+	loadedImages = []
+	for image in imageList:
+		img = Image.open(path + '/' + image)
+		img = np.asarray(img)
+		loadedImages.append(img)
+
+		return loadedImages
+
+
 
 def generator_score():
 	#print generator loss in the network
 	#print(-vphi_fake.mean())
 	pass
 
-def get_inception_score(images, splits=10):
-	assert(type(images) == list)
-	assert(type(images[0]) == np.ndarray)
-	assert(len(images[0].shape) == 3)
+def inception_score(imgs, cuda=gpu, batch_size=32, resize=False):
+	"""Computes the inception score of the generated images imgs
+		imgs -- list of (HxWx3) numpy images normalized in the range [0,1]
+		cuda -- whether or not to run on GPU
+		batch_size -- batch size to feed into inception
+		https://github.com/sbarratt/inception-score-pytorch/blob/master/inception_score.py
+		"""
 
-	#gathering inputs
-	inputs = []
-	for img in images:
-		img = img.astype(np.float32)
-		inputs.append(np.expand_dims(img, 0))
-	
-	#batches
-	bs = 100
-	n_bathces = int(math.ceil(float(len(inps))/float(bs)))
+	N = len(imgs)
 
-	#get predictions
-	preds = []
-	for i in range(n_batches):
-		sys.stdout.write(".")
-		sys.stdout.write(str(i))
-		sys.stdout.flush()
+	assert batch_size > 0
+		
+	assert N > batch_size
 
-		inp = inputs[(i * bs):min((i + 1) * bs, len(inputs))]
-		inp = np.concatenate(inp, 0)
-		pred = nn.Softmax(inp)
-		preds.append(pred)
-	preds = np.concatenate(preds, 0)
 
-	#get score
+	if cuda:
+		dtype = torch.cuda.FloatTensor
+
+	else:
+		if torch.cuda_isavailable():
+			print("You have an unused CUDA device")
+
+		dtype = torch.FloatTensor
+
+	dataloader = torch.utils.data.DataLoader(imgs, batch_size=batch_size)
+
+
+	#load model
+	inception_model = inception_v3(pretrained=True, transform_input=True).type(dtype)
+	inception_model.eval()
+	up = nn.Upsample(size=(300,300), mode='bilinear').type(dtype)
+
+	def get_pred(x):
+		if resize:
+			x = up(x)
+
+			x = inception_model(x)
+
+		return F.softmax(x)
+
+	#predictions
+	preds = np.zeros((N,1000))
+
+		#class ImageDataSet(torch.utils.data.Dataset):
+			#def __init__(self, imgs):
+				#self.imgs = imgs
+
+			#def __getitem__(self, index):
+				#return self.imgs[index]
+
+			#def __len(self):
+				#return len(self.imgs)
+
+	imgs_data = ImageDataSet(imgs)
+
+	for i, batch in enumerate(dataloader, 0):
+		batch = batch.type(dtype)
+		bathcv = Variable(batch)
+		batch_size_i = batch.size()[0]
+
+		p_out = get_pred(batchv)
+		preds[i*batch_size:i*batch_size + batch_size_i] = p_out.data.cpu().numpy()
+
+
+	#compute mean k1-div
+	py = np.mean(preds, axis=0)
 	scores = []
-	for i in range(splits):
-		part = preds[(i * preds.shape[0] // splits):((i + 1) * preds.shape[0] // splits), :]
-		kl = part * (np.log(part) - np.log(np.expand_dims(np.mean(part, 0), 0)))
-		kl = np.mean(np.sum(kl,1))
-		scores.append(np.exp(kl))
+	for i in range(preds.shape[0]):
+		pyx = preds[i, :]
+		scores.append(entropy(pyx, py))
+		mean_kl = np.mean(scores)
 
-	return np.mean(score), np.std(score)
+	return np.exp(mean_kl)
 
 
 def ssl_performance():
-	pass
+		pass
+
+
+imgs = load_images(folder)
+print(inception_score(imgs))
 
 
 
-		
+
 
