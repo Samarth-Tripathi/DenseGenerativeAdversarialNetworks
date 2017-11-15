ad3139b5cd0da913a9ddbd15854f2e4154b9b68d
Renbo <rt2632@columbia.edu>
2017-11-15 00:49:20 -0500

    tested inception

--- evaluate.py
+++ evaluate.py
@@ -12,6 +12,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.data
 from torchvision.models.inception import inception_v3
+from torch.autograd import Variable
 
 import sys
 import argparse
@@ -27,7 +28,7 @@ parser.add_argument('--dataroot', required=True, help='path to the images')
 parser.add_argument('--cuda', default=False, help='whether to use gpu or not')
 opt = parser.parse_args()
 
-#print(opt)
+print(opt)
 
 folder = opt.dataroot
 gpu = opt.cuda
@@ -36,13 +37,18 @@ def load_images(path):
 	#return image array
 
 	imageList = os.listdir(path)
+
 	loadedImages = []
 	for image in imageList:
 		img = Image.open(path + '/' + image)
 		img = np.asarray(img)
+		#print(img.shape)
+		img = np.transpose(img, (2, 0, 1))
+		img = img/255.0
+		#print(img.shape)
 		loadedImages.append(img)
 
-		return loadedImages
+	return loadedImages
 
 
 
@@ -51,7 +57,7 @@ def generator_score():
 	#print(-vphi_fake.mean())
 	pass
 
-def inception_score(imgs, cuda=gpu, batch_size=32, resize=False):
+def inception_score(imgs, cuda=gpu, batch_size=2, resize=False):
 	"""Computes the inception score of the generated images imgs
 		imgs -- list of (HxWx3) numpy images normalized in the range [0,1]
 		cuda -- whether or not to run on GPU
@@ -60,8 +66,10 @@ def inception_score(imgs, cuda=gpu, batch_size=32, resize=False):
 		"""
 
 	N = len(imgs)
+	print(imgs[0].shape)
 
 	assert batch_size > 0
+
 		
 	assert N > batch_size
 
@@ -70,7 +78,7 @@ def inception_score(imgs, cuda=gpu, batch_size=32, resize=False):
 		dtype = torch.cuda.FloatTensor
 
 	else:
-		if torch.cuda_isavailable():
+		if torch.cuda.is_available():
 			print("You have an unused CUDA device")
 
 		dtype = torch.FloatTensor
@@ -81,13 +89,13 @@ def inception_score(imgs, cuda=gpu, batch_size=32, resize=False):
 	#load model
 	inception_model = inception_v3(pretrained=True, transform_input=True).type(dtype)
 	inception_model.eval()
-	up = nn.Upsample(size=(300,300), mode='bilinear').type(dtype)
+	up = nn.Upsample(size=(299,299), mode='bilinear').type(dtype)
 
 	def get_pred(x):
 		if resize:
 			x = up(x)
 
-			x = inception_model(x)
+		x = inception_model(x)
 
 		return F.softmax(x)
 
@@ -104,11 +112,11 @@ def inception_score(imgs, cuda=gpu, batch_size=32, resize=False):
 			#def __len(self):
 				#return len(self.imgs)
 
-	imgs_data = ImageDataSet(imgs)
+	#imgs_data = ImageDataSet(imgs)
 
 	for i, batch in enumerate(dataloader, 0):
 		batch = batch.type(dtype)
-		bathcv = Variable(batch)
+		batchv = Variable(batch)
 		batch_size_i = batch.size()[0]
 
 		p_out = get_pred(batchv)
@@ -132,6 +140,24 @@ def ssl_performance():
 
 imgs = load_images(folder)
 print(inception_score(imgs))
+
+'''if __name__ == '__main__':
+    print ("Generating images...")
+    class ImageDataset(torch.utils.data.Dataset):
+        def __init__(self, imgs):
+            self.imgs = imgs
+
+        def __getitem__(self, index):
+            return self.imgs[index]
+
+        def __len__(self):
+            return len(self.imgs)
+    imgs = [np.random.uniform(0, 1, size=(3, 32, 32)).astype(np.float32) for _ in range(10)]
+    imgs_dset = ImageDataset(imgs)
+
+    print ("Calculating Inception Score...")
+    print (inception_score(imgs_dset, cuda=False, batch_size=2, resize=True))
+    '''
 
 
 
